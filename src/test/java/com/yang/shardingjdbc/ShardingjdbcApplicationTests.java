ddfc39664124f95b7bfd0b468d4cf6a121b9eb2b
yangchao <909833655@qq.com>
2023-05-27 06:46:01 +0800

    Initial commit

---
+++ src/test/java/com/yang/shardingjdbc/ShardingjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.shardingjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
